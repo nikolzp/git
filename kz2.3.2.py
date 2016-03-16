1684a80189b9355e3aac26223028e6eb34ceaee6
Nikolz <nikolzp@gmail.com>
2016-03-16 12:06:16 +0200

    work

--- kz2.3.2.py
+++ kz2.3.2.py
@@ -1,25 +1,19 @@
 def count_holes(value):
-    count = 0
-    li = []
-    if len(value) == 0:
+    if type(value) is not int and type(value) is not str:
         return 'error'
     value = str(value)
-    if value is not int:
-        for a in filter(str.isdigit, value): li.append(a)
-    if li[0] == '0':
-        del li[0]
-    for a in li:
+    if value.isalpha(): return 'error'
+    elif len(value) == 0: return 'error'
+    elif '.' in value: return 'error'
+    
+    count = 0
+    s = ''
+    for a in filter(str.isdigit, value): s += a
+    s = str(abs(int(s)))
+    for a in s:
         a = int(a)
         if a == 8:
             count = count + 2
         elif a == 0 or a==4 or a==6 or a==9:
             count +=1
-    for a in value:
-        if a == '.':
-            return 'error'
-    if not value.isalnum():
-        return 'error'    
     return count
-  
-r = count_holes('')
-print r
