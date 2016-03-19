3c46bc60d89897f9c02d35d44ece0472a76c5831
nikolz_lin_Home <nikolzp@gmail.com>
2016-03-20 00:46:33 +0200

    it works

--- kz2.4.3.py
+++ kz2.4.3.py
@@ -2,29 +2,19 @@ def find_most_frequent(text):
     if text == '':
         return []
     text = text.lower()
-    red_text = ''
-    lis_povt = []
-    fin_list = []
-    
     znaki = [',', '.', ':', ';', '!', '?']
-    for a in text.split(' '):
-        if a[-1] in znaki:
-            a = a[:-1] 
-        red_text += a + ' '
+    lis = [] 
+    dic = {} 
     
     for a in text.split():
-        print a
-        if text.count(a) > 1:
-            list_povt = lis_povt.append(a)
-    
-    for a in lis_povt:
-        if a in fin_list: continue
-        fin_list.append(a)
-    return sorted(fin_list)
-            
+        if a[-1] in znaki:
+            a = a[:-1]
+        lis.append(a)
 
-
-
-text = "And I forget just why I taste; Oh yeah, I guess it makes me smile; I found it hard; it's hard to find; Oh well, whatever, never mind."
-s = find_most_frequent(text)
-print s
+    for a in lis:
+        if a not in dic:
+            dic[a] = a.count(a)
+        else:
+            dic[a] += a.count(a)
+    ma = max(dic.values())
+    return sorted([a for a in dic if dic[a] == ma])
