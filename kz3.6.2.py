947f73bb0fafa51e203622ca5a6d4ec88f3aa231
nikolz_lin_Home <nikolzp@gmail.com>
2016-03-26 00:43:19 +0200

    it_works

--- kz3.6.2.py
+++ kz3.6.2.py
@@ -1,3 +1,4 @@
+import csv
 class Student(object):
     def __init__(self, name, conf={'lab_max':0, 'lab_num':0}):
         self.name = name
@@ -22,4 +23,25 @@ class Student(object):
     def average_score(self):
         okruglenie = sum(self.labs)/float(len(self.labs))
         return round(okruglenie, 1)
+
+
+def find_best_student(filename):
+    dic = {} 
+    fp = open(filename)
+    fp.readline()
+    fp_csv = csv.reader(fp)
+    for a in fp_csv:
+        s = Student(a[0], {'lab_max':int(a[1]), 'lab_num':int(a[2])})
+        for r in range(s.conf.get('lab_num')):
+            ma = {s.name: sum(s.set_lab(int(a[r+3]), r))}
+            dic.update(ma)
+    fp.close()
+    name_val = max(dic.values())
+    name = [a for a in dic if dic[a]==name_val]
+    return name[0]
     
+
+
+p = find_best_student('student.csv')
+print p
+
